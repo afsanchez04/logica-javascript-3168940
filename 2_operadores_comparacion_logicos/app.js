6e7e5a30f1e5c284007e9c6cda4aace117434ed7
Andrés Sánchez <38230339+afsanchez04@users.noreply.github.com>
2025-07-31 13:27:29 -0500

    ejercicio terminado

--- 2_operadores_comparacion_logicos/app.js
+++ 2_operadores_comparacion_logicos/app.js
@@ -18,6 +18,17 @@ console.log( 5 + 3 ) // 8
 //Pida dos números al usuario y sumelos
 let num1 = Number( prompt('Dame num 1') )  //"5"
 let num2 = Number( prompt('Dame num 3') ) //"3"
-let resultado = num1 + num2 
-alert( "El resultado de tu suma es " + resultado )
+let suma = num1 + num2 
+let resta = num1 - num2 
+let multiplicacion = num1 * num2 
+let division = num1 / num2 
+
+//Solución del ejercicio concatenando (+)
+/* console.log('La suma de ' + num1 + ' + ' + num2 + ' = ' + suma)
+console.log('La resta de ' + num1 + ' - ' + num2 + ' = ' + resta)
+console.log('La multiplicacion de ' + num1 + ' * ' + num2 + ' = ' + multiplicacion)
+console.log('La division de ' + num1 + ' / ' + num2 + ' = ' + division) */
+
+//
+console.log(`Tu número es ${num1}`)
 
