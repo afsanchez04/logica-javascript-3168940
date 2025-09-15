0bd6cac98974301cd46e1ff4e4bdd2c44abf07c8
afsaanchez04 <afsanchez04@gmail.com>
2025-09-15 12:42:54 -0500

    actualizacion galería 1

--- 8_Galerias_Lightbox/galeria_1/app.js
+++ 8_Galerias_Lightbox/galeria_1/app.js
@@ -2,15 +2,29 @@ const titulo = document.querySelector(".titulo")
 const contenedor = document.querySelector("#contenedor")
 const btnPrev = document.querySelector(".btn-prev")
 const btnNext = document.querySelector(".btn-next")
+const titulos = [
+  "Primera imagen",
+  "Segunda imagen",
+  "Tercera imagen",
+  "Cuarta imagen",
+  "Quinta imagen",
+]
 let i = 1
 
-function prev(){
+function prev() {
   console.log("Anterior....")
 }
 
-function next(){
-  console.log("Siguiente....")
+function next() {
+  console.log(i)
+  if (i === 5) {
+    i = 0
+  }
+  titulo.textContent = titulos[i]
+  contenedor.style.marginLeft = `-${400 * i}px`
+  contenedor.style.transitionDuration = ".5s"
+  i++ //i=1
 }
 
-btnPrev.addEventListener("click",prev)
-btnNext.addEventListener("click",next)
+btnPrev.addEventListener("click", prev)
+btnNext.addEventListener("click", next)
