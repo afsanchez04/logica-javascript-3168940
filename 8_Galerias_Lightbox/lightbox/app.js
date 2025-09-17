7bb00482b3cc7c24d057bb61c41676a503a7183b
afsaanchez04 <afsanchez04@gmail.com>
2025-09-17 13:22:10 -0500

    instrucciones lightbox

---
+++ 8_Galerias_Lightbox/lightbox/app.js
@@ -0,0 +1,21 @@
+const flotante = document.querySelector("#flotante")
+const cuadro = document.querySelector("#cuadro")
+const equis = document.querySelector("#equis")
+const sig = document.querySelector("#sig")
+const ant = document.querySelector("#ant")
+const imgprincipal = document.querySelector("#imgprincipal")
+const fath = document.querySelector("#fath")
+
+function mostrarLightbox () {
+  //Cambiar top a 0px (cuando se haga click en alguna miniatura)
+}
+function ocultarLightbox () {
+  //Cambiar top a -100vh (cuando se haga click en equis o cuadro)
+}
+function siguiente (){
+  //Cambia a imagen 2
+}
+function anterior (){
+  //Cambia a imagen 1
+}
+//Falta crear los eventos con addEventListener
